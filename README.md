45bdef945581b773bf8f2ccd5aaf9864932a5796
Abdihakim Hassan <h_abdihakimi@yahoo.com>
2020-03-25 14:49:41 +0300

    Finished scripts file

---
+++ README.md
@@ -0,0 +1,20 @@
+# Calcutator
+#### Calculator App, 23/03/2020
+#### By **Abdihakim**
+## Description
+Built for Practice and testing  my newly learnt function skills in java script .
+## Setup/Installation Requirements
+* technical knowledge is an advantage
+* internet connection
+* follow this link https:
+* either clone the site or download the zip file
+## Known Bugs
+Nil
+## Technologies Used
+HTML5
+Js
+## Support and contact details
+h_abdihakim1@outlook.com or ibrahimabdihakim59@gmail.com
+### License
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+Copyright (c) 2020 **Abdihakim**
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+
+// Addittion
+function add(num1, num2) {
+    return num1 + num2;
+  };
+ var num1 = parseInt (prompt("Enter a number:"));
+  var num2 = parseInt(prompt("Enter another number:"));
+  var result = add(num1, num2);
+   alert(result);
+
+
+//   Subtraction 
+
+function subtract(num1, num2) {
+    return num1 - num2;
+  };
+ var num1 = parseInt (prompt("Enter a number:"));
+  var num2 = parseInt(prompt("Enter another number:"));
+  var result = subtract(num1, num2);
+   alert(result);
+
+
+// Multiplication
+
+
+function multiply(num1, num2) {
+    return num1 * num2;
+  };
+ var num1 = parseInt (prompt("Enter a number:"));
+  var num2 = parseInt(prompt("Enter another number:"));
+  var result= multiply(num1,num2);
+  alert(result);
+
+
+
+// Division
+
+
+var divide= function(num1, num2){
+    return num1 / num2;
+  };
+  var num1= parseInt (prompt("Enter your number 1:")); 
+  var num2= parseInt (prompt("Enter your number 2:"));
+  var result= divide(num1,num2);
+  alert(result);
+
